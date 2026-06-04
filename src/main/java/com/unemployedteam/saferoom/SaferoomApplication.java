7d05ccde371ce9a9837844aded5afcade2e3b87d
Hyun-jun-Lee0811 <korong0811@naver.com>
2026-06-04 21:35:51 +0900

    feat: 초기 세팅

---
+++ src/main/java/com/unemployedteam/saferoom/SaferoomApplication.java
@@ -0,0 +1,13 @@
+package com.unemployedteam.saferoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaferoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaferoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unemployedteam/saferoom/SaferoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unemployedteam.saferoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaferoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
