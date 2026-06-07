d866b2cf6ac45990017817f3ccd11f17b74f9820
Hyun-jun-Lee0811 <korong0811@naver.com>
2026-06-08 01:41:45 +0900

    refactor: UserResponse 생성자 어노테이션 추가 - NoArgsConstructor & AllArgsConstructor

--- src/main/java/com/unemployedteam/saferoom/user/UserResponse.java
+++ src/main/java/com/unemployedteam/saferoom/user/UserResponse.java
@@ -1,11 +1,15 @@
 package com.unemployedteam.saferoom.user;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponse {
 
   private Long userId;
