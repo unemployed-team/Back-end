33435723f630310c3d08eea95fa6ba1bb41d0e78
Hyun-jun-Lee0811 <korong0811@naver.com>
2026-06-08 01:43:48 +0900

    refactor: UserRequest 생성자 어노테이션 추가

--- src/main/java/com/unemployedteam/saferoom/user/UserRequest.java
+++ src/main/java/com/unemployedteam/saferoom/user/UserRequest.java
@@ -1,8 +1,14 @@
 package com.unemployedteam.saferoom.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequest {
 
   private String nickname;
