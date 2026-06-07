84a756f48fb17ab9093c499002d15033b5a7f7fa
Hyun-jun-Lee0811 <korong0811@naver.com>
2026-06-07 18:18:49 +0900

    refactor: OAuth 인증을 위한 경로 필터링 추가

--- src/main/java/com/unemployedteam/saferoom/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/unemployedteam/saferoom/auth/JwtAuthenticationFilter.java
@@ -27,6 +27,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   protected void doFilterInternal(HttpServletRequest request,
       HttpServletResponse response,
       FilterChain chain) throws ServletException, IOException {
+
+    String path = request.getRequestURI();
+    if (path.startsWith("/auth")) {
+      chain.doFilter(request, response);
+      return;
+    }
+
     String authHeader = request.getHeader("Authorization");
 
     if (authHeader == null || !authHeader.startsWith("Bearer ")) {
