432f7d0e2432a3dea0a230ccd8ccdb7f2bf27ec8
Hyun-jun-Lee0811 <korong0811@naver.com>
2026-06-28 15:37:09 +0900

    refactor: permitAll() 범위 축소 (보안 버그)

--- src/main/java/com/unemployedteam/saferoom/global/config/SecurityConfig.java
+++ src/main/java/com/unemployedteam/saferoom/global/config/SecurityConfig.java
@@ -34,10 +34,7 @@ public class SecurityConfig {
                 "/v3/api-docs/**",
                 "/buildings/**",
                 "/field-reports/**",
-                "/hri/**",
-                "/bookmarks/**",
-                "/contracts/**",
-                "/field-reports/*/submit"
+                "/hri/**"
             ).permitAll()
             .anyRequest().authenticated()
         )
